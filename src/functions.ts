d523c44d46e78b70d12ff0673c823e93416a391d
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2021-11-22 10:02:25 +0100

    Added functions

---
+++ src/functions.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 - All rights reserved.
+ * https://gigadrivegroup.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+/**
+ * Finds a DOM element by its selector.
+ * @param selector The selector to use
+ */
+export function node(selector: string): Element {
+	return document.querySelector(selector);
+}
+
+/**
+ * Gets an attribute of an element
+ * @param element The element to search
+ * @param name The name of the attribute to find
+ */
+export function attr(element: Element, name: string): string | null {
+	return element.getAttribute(name);
+}
